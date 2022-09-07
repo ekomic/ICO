01989222e1856579cc0cf3ddff2f172c810a8e1b
Christopher Ekom <ekomchris2016@gmail.com>
2022-09-07 04:15:32 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CHRIS_NFT_CONTRACT_ADDRESS = "0xc3529709Df79518721e68Ff23abD56c362b2970F";
+
+module.exports = { CHRIS_NFT_CONTRACT_ADDRESS };
